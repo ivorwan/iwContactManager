e79ffca782d5f303f2be66de49d43c54a3cb8090
ivorwan <riogangsta@gmail.com>
2014-08-28 18:28:09 -0700

    initial commit

---
+++ iwContactManager/Models/List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iwContactManager.Models
+{
+    public class List
+    {
+        public int ListId { get; set; }
+        public string ListName { get; set; }
+        public string ListDescription { get; set;  }
+    }
+}
---
+++ iwContactManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iwContactManager.Startup))]
+namespace iwContactManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
