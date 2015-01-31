fee4852a04ed4790bf925d9992d9cc4aa9d55c53
ivorwan <riogangsta@gmail.com>
2015-01-30 19:31:40 -0800

    signalR + angular chat

--- iwContactManager/Scripts/App.js
+++ iwContactManager/Scripts/App.js
@@ -1,11 +1,78 @@
 ﻿(function () {
     var app = angular.module('iwContactManager', ['ngAnimate']);
+    app.value('$', $);
+
+    app.service('signalRSvc', function ($, $rootScope) {
+        var proxy = null;
+
+        var initialize = function () {
+            //Getting the connection object
+            connection = $.hubConnection();
+
+            //Creating proxy
+            this.proxy = connection.createHubProxy('chatHub');
+
+            //Starting connection
+            connection.start();
+            //Publishing an event when server pushes a greeting message
+            this.proxy.on('broadcastMessage', function (name, message) {
+                $rootScope.$emit("broadcastMessage", name, message);
+            });
+        };
+
+        var sendRequest = function (name, message) {
+            //console.log('sendRequest - name: ' + name + ' - message: ' + message);
+            //Invoking greetAll method defined in hub
+            this.proxy.invoke('Send', name, message);
+        };
+
+        return {
+            initialize: initialize,
+            sendRequest: sendRequest
+        };
+    });
+
+
+    
+    app.controller('ChatController', function ($scope, signalRSvc, $rootScope) {
+    
+        //console.log('chat controller');
+        $scope.discussion = [];
+
+        $scope.submitName = function () {
+            //console.log('sumit name');
+        }
+        $scope.greetAll = function () {
+            //console.log('greetAll - $scope.name: ' + $scope.name + ' this.name: ' + this.name + '- $rootScope.message: ' + $rootScope.message);
+            signalRSvc.sendRequest(this.name, this.message);
+        }
+
+        updateGreetingMessage = function (name, message) {
+            //console.log('updateGreetingMessage');
+            var msg = { "name": name, "message": message };
+            //console.log('pushing msg: ' + msg.name + ' : ' + msg.message);
+            $scope.discussion.push(msg);
+        }
+
+        signalRSvc.initialize();
+
+        //Updating greeting message after receiving a message through the event
+
+        $scope.$parent.$on("broadcastMessage", function (e, name, message) {
+            $scope.$apply(function () {
+                //console.log('broadcastMessage - name: ' + name + ' - message: ' + message);
+                updateGreetingMessage(name, message);
+            });
+        });
+        
+    });
+    
 
     app.directive('noticeDirective', function () {
         return {
             scope: {
                 index: '@',
-                isolatedBindingWinners: '=bindingWinners'
+                //isolatedBindingWinners: '=bindingWinners'
             },
             link: function (scope, element, attrs) {
             }
